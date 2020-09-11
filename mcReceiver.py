4fbc3a8ea644bface169735c6ef23e5b3b53484b
Lehmann Tzahi <TC02993@esl.corp.elbit.co.il>
2020-09-11 10:54:19 +0300

    Added multicast sender+receiver

---
+++ mcReceiver.py
@@ -0,0 +1,21 @@
+import socket
+import struct
+
+multicastIP = '224.1.1.1'
+multicastPort = 5007
+IS_ALL_GROUPS = True
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+if IS_ALL_GROUPS:
+  # on this port, receives ALL multicast groups
+  sock.bind(('', multicastPort))
+else:
+  # on this port, listen ONLY to multicastIP
+  sock.bind((multicastIP, multicastPort))
+msgReq = struct.pack("4sl", socket.inet_aton(multicastIP), socket.INADDR_ANY)
+
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, msgReq)
+
+while True:
+  print(sock.recv(10240))
